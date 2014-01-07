70652b42fa574c030bb6a0690264780c6e67f14b
Ryo Tagami <ultrafortress@gmail.com>
2014-01-08 03:25:58 +0900

    Refactoring.
    Refactored following:
    * Variable names.
    * optparse help strings.
    * Function names.
    * Function arguments and return values.
    * Conditional branching.

--- zabbix_media_hipchat.py
+++ zabbix_media_hipchat.py
@@ -14,11 +14,15 @@ import urllib2
 
 
 def main():
-    arguments = return_options_and_arguments()
-    (room, params, message) = arguments
+    arguments = get_arguments()
+    room = arguments['room']
+    auth_token = arguments['auth_token']
+    status = arguments['status']
+    severity = arguments['severity']
+    message = arguments['message']
 
     json_body_dict = {}
-    json_body_dict['color'] = return_color(params[1], params[2])
+    json_body_dict['color'] = return_color(status, severity)
     json_body_dict['message'] = return_message_body(message)
     json_body_dict['notify'] = True
     json_body_dict['message_format'] = 'text'
@@ -28,7 +32,7 @@ def main():
     opener_director = urllib2.build_opener(handler)
     request = urllib2.Request(API_ENDPOINT_ROOM.format(room))
     request.add_data(json_body_str)
-    request.add_header('Authorization', 'Bearer {0}'.format(params[0]))
+    request.add_header('Authorization', 'Bearer {0}'.format(auth_token))
     request.add_header('Content-Type', 'application/json')
 
     try:
@@ -52,72 +56,79 @@ class PlainTextEpilogFormatter(optparse.IndentedHelpFormatter):
             return ""
 
 
-def return_options_and_arguments():
-    optparse_usage = '%prog [options] "auth_token" "to" "subject" "message"'
+def get_arguments():
+    usage = '%prog [options] "room" "params" "message"'
 
-    optparse_version = '%prog {0}'.format(__version__)
+    version = '%prog {0}'.format(__version__)
 
-    optparse_description = textwrap.dedent('''\
+    description = textwrap.dedent('''\
         Send alert to HipChat room mentioning everyone.
     ''')
 
-    optparse_epilog = textwrap.dedent('''\
+    epilog = textwrap.dedent('''\
         positional arguments:
-            room            ID of the room which message will be sent to.
+            room            ID of the room which the message is sent to.
             params          Comma separated values of following(in order):
                 auth_token      Bearer token to authenticate API access against
-                                HipChat API version 2.
-                status          Severity of the alert. Either one of default
+                                HipChat API version 2. Required.
+                status          Status of the alert. 'OK' will set the
+                                background color to 'green' irrespective of
+                                the severity of the alert. If not specified, or
+                                anything else than 'OK' was specified, nothing
+                                will happen.
+                severity        Severity of the alert. Either one of default
                                 verbal severities defined by zabbix ('Average',
                                 'High', ...) or numerical severity defined by
                                 zabbix (0 <= n <= 5). Background color of the
                                 message sent to HipChat will be set according
-                                to this option.  If not specified, 'High' is
+                                to this value.  If not specified, 'High' is
                                 used as a default.
-                severity        Status of the alert. 'OK' will set the
-                                background color to 'green' irrespective of
-                                the severity of the alert. If not specified, or
-                                anything else than 'OK' was specified,
-                                'PROBLEM' is used as a default.
-                            Latter one or two values can be omitted.
+                            Latter two values can be omitted.
                             Example: 'token123:PROBLEM:Disaster'
-            message        Rest of the body of the message.
+            message         Rest of the body of the message.
     ''')
 
     option_parser = optparse.OptionParser(
-        usage=optparse_usage,
-        version=optparse_version,
-        description=optparse_description,
+        usage=usage,
+        version=version,
+        description=description,
         formatter=PlainTextEpilogFormatter(),
-        epilog=optparse_epilog,
+        epilog=epilog,
     )
 
-    (_, arguments) = option_parser.parse_args()
+    (_, parsed_arguments) = option_parser.parse_args()
 
-    if len(arguments) != 3:
+    returned_arguments = {}
+
+    if len(parsed_arguments) != 3:
         option_parser.print_help()
         sys.exit(2)
     else:
-        (room, params, message) = arguments
+        returned_arguments['room'] = parsed_arguments[0]
+        params = parsed_arguments[1].split(',')
+        returned_arguments['message'] = parsed_arguments[2]
 
-    params = params.split(',')
-
-    if len(params) == 1:
-        params.append(None)
-        params.append(None)
-    elif len(params) == 2:
-        params.append(None)
-    elif len(params) == 3:
-        pass
-    else:
+    if not 1 <= len(params) <= 3:
         option_parser.print_help()
         sys.exit(2)
 
-    return (room, params, message)
+    returned_arguments['auth_token'] = params[0]
+
+    try:
+        returned_arguments['status'] = params[1]
+    except IndexError:
+        returned_arguments['status'] = None
+
+    try:
+        returned_arguments['severity'] = params[2]
+    except IndexError:
+        returned_arguments['severity'] = None
+
+    return returned_arguments
 
 
 def return_color(status, severity):
-    msg_color_map_verbal = {
+    verbal_severity_color_map = {
         'Not classified': 'gray',
         'Information': 'purple',
         'Warning': 'yellow',
@@ -126,7 +137,7 @@ def return_color(status, severity):
         'Disaster': 'red',
     }
 
-    msg_color_map_numerical = {
+    numerical_severity_color_map = {
         0: 'gray',
         1: 'purple',
         2: 'yellow',
@@ -139,12 +150,12 @@ def return_color(status, severity):
         color = 'green'
     else:
         try:
-            color_from_verbal = msg_color_map_verbal[severity]
+            color_from_verbal = verbal_severity_color_map[severity]
         except KeyError:
             color_from_verbal = None
 
         try:
-            color_from_numerical = msg_color_map_numerical[severity]
+            color_from_numerical = numerical_severity_color_map[severity]
         except KeyError:
             color_from_numerical = None
 
